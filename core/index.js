6fa473cf7cf2d91974f377e94ae100af31a59769
Eugen Ciur <eugen@papermerge.com>
2021-07-08 20:21:49 +0200

    open dual commander in different modes

--- core/index.js
+++ core/index.js
@@ -57,6 +57,25 @@ function left_panel_from_url(url_as_string) {
     much as it can) { commander: true, folder: undefined }.
     Undefined folder tells commander to open root folder.
     */
+    let regexp = /\/core\/folder\/([0-9]+)/,
+        _match,
+        ret = undefined,
+        folder_id = undefined;
+
+    _match = url_as_string.match(regexp);
+
+    if (_match) {
+        ret = {
+            commander: true,
+            folder: { id: _match[1] }
+        };
+    } else {
+        ret = {
+            commander: true
+        };
+    }
+
+    return ret;
 }
 
 function right_panel_from_url(url_as_string) {
@@ -75,10 +94,24 @@ function right_panel_from_url(url_as_string) {
     In case of undefined return value, it signals that
     right side panel will start as closed.
     */
+    let regexp = /\/core\/folder\/([0-9]+)/,
+        _match,
+        ret = undefined;
+
+    _match = url_as_string.match(regexp);
+
+    if (_match) {
+        ret = {
+            commander: true,
+            folder: { id: _match[1] }
+        };
+    }
+
+    return ret;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    let dual_commander;
+    let dual_commander, left, right;
 
 
     dual_commander = new DualCommanderView({
@@ -86,10 +119,23 @@ document.addEventListener("DOMContentLoaded", () => {
         'panel_right': {'el': '#panel_right'},
     });
 
+    left = left_panel_from_url(window.location.pathname);
+    console.log(`left = ${left}`);
+
+    right = right_panel_from_url(window.location.search);
+    console.log(` right= ${right}`);
+
     dual_commander.open({
-        left: {commander: true},
-        right: {commander: true},
+        left: left,
+        right: right
     });
+
+    if (!right) {
+        dual_commander.on_switch_2_single(
+            dual_commander.commander_right,
+            dual_commander.commander_left,
+        );
+    }
 
     window.addEventListener('resize', adjust_panels_height);
     adjust_panels_height();
